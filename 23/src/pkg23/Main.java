6def39d6e13fd2de2bba0d9d852a9650bbc662d2
18PROGB0026 <18PROGB0026@TS-LABS-2.labs.cbtis94.local>
2019-10-03 13:51:15 -0500

    Terminamos el programa, agregamos los datos de la interfaz

--- 23/src/pkg23/Main.java
+++ 23/src/pkg23/Main.java
@@ -1,10 +1,32 @@
 package pkg23;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        Libro nuevo = new Libro();
+        Scanner leer = new Scanner(System.in);
+
+        System.out.print("Ingresa el nombre del Autor:");
+        nuevo.setAutor(leer.nextLine());
+        System.out.print("Ingresa el nombre del libro:");
+        nuevo.setTitulo(leer.nextLine());
+        System.out.print("Ingresa numero:");
+        nuevo.setNumero(leer.nextInt());
+        System.out.print("Ingresa numero existencia:");
+        nuevo.setNumeroexistencia(leer.nextInt());
+        System.out.print("Ingresa numero prestados:");
+        nuevo.setNumeroprestados(leer.nextInt());
+
+        System.out.println(nuevo.toString());
+        if (nuevo.Prestamo()) {
+            System.out.println("libro prestado");
+        } else {
+            System.out.println("libro no prestado");
+        }
+
     }
 
 }
