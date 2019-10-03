3520703266c407a4f9fd09542515f35ef41a5729
18PROGB0026 <18PROGB0026@TS-LABS-2.labs.cbtis94.local>
2019-10-03 11:37:10 -0500

    Codigo base del proyecto, se creo la clase LIBRO

---
+++ 23/src/pkg23/Libro.java
@@ -0,0 +1,20 @@
+package pkg23;
+
+public class Libro {
+
+    //atributos
+    private String Titulo;
+    private String Autor;
+    private int Número ;
+
+    public Libro() {
+    }
+    
+    
+    
+    /**
+     * costructor vacio
+     */
+    
+    
+}
