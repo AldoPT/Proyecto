b3933faa1f2d62ee0c314f43430fe17da7509d70
18PROGB0026 <18PROGB0026@TS-LABS-2.labs.cbtis94.local>
2019-10-03 12:57:01 -0500

    Se crearon los parametro, y se agrego los set y get

--- 23/src/pkg23/Libro.java
+++ 23/src/pkg23/Libro.java
@@ -5,22 +5,76 @@ public class Libro {
     //atributos
     private String Titulo;
     private String Autor;
-    private int Número;
+    private int Numero;
+    private int Numeroexistencia;
+    private int Numeroprestados;
 
     /**
-     * costructor vacio
+     * Parametro vacio
      */
     public Libro() {
     }
 
-    /**
-     * Constructor con parametros
+    /*
+    * Parametro 
      */
-
-    public Libro(String Ti, String Au, int Num) {
-        this.Titulo = Ti;
-        this.Autor = Au;
-        this.Número = Num;
+    public Libro(String Titulo, String Autor, int Numero, int Numeroexistencia, int Numeroprestados) {
+        this.Titulo = Titulo;
+        this.Autor = Autor;
+        this.Numero = Numero;
+        this.Numeroexistencia = Numeroexistencia;
+        this.Numeroprestados = Numeroprestados;
     }
 
+    /**
+     * Creamos Set y Get
+     * @return 
+     */
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public int getNumeroexistencia() {
+        return Numeroexistencia;
+    }
+
+    public void setNumeroexistencia(int Numeroexistencia) {
+        this.Numeroexistencia = Numeroexistencia;
+    }
+
+    public int getNumeroprestados() {
+        return Numeroprestados;
+    }
+
+    public void setNumeroprestados(int Numeroprestados) {
+        this.Numeroprestados = Numeroprestados;
+    }
+
+    
+    public int Prestamo(){
+        int M=0;
+      
+        return 0;
+        
+    }
 }
